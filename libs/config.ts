5ebbf92f42b571d53c5849d3c49407d6f9a96aa6
dilshod-14 <urolovdilshod24@gmail.com>
2025-10-06 17:37:19 +0900

    fix: config faylidagi NEXT_PUBLIC_API_URLni ozgartirdik

--- libs/config.ts
+++ libs/config.ts
@@ -1,4 +1,6 @@
 export const REACT_APP_API_URL = `${process.env.REACT_APP_API_URL}`;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
 
 export const availableOptions = ['propertyBarter', 'propertyRent'];
 
